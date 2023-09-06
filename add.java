16c0d7ab395992fa8f6a65a0b3cb899315e5684a
ArsalanCoder <arsalanriyazuddinkhan@gmail.com>
2023-09-06 11:51:46 +0530

    Done BY Arsalan

--- add.java
+++ add.java
@@ -1,5 +1,14 @@
 import java.util.Scanner;
+class operator
+{
+    public int add()
+    {
+        int a=22;
+        int b=14;
+        return(a+b);
 
+    }
+}
 class first
 {
     public static void main(String[] args) {
@@ -7,6 +16,10 @@ class first
     Scanner sc=new Scanner(System.in);
     String a=sc.next();
     System.out.println("Hello "+a);
-    
+    sc.close();
+    System.out.println("Abdul Rehman");
+    operator o=new operator();
+    double sum=o.add();
+    System.out.println(sum);
     }
 }
